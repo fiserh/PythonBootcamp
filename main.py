1ecb0dcea673e205c0d787218473ac6b7c7966ba
Jan Fiser <jan.fiser2@akka.eu>
2021-02-02 17:49:40 +0100

    Snake basic

--- main.py
+++ main.py
@@ -9,15 +9,33 @@ screen.bgcolor("black")
 turtles = []
 
 tim = t.Turtle()
-tim.shape("square")
+tim.shape("circle")
 tim.color("white")
 tim.pensize()
 tim.pensize(20)
+tim.speed(1)
 
-start_shape = [(0, 0), (0, 20)]
+start_shape = [(0, 0), (-20, 0), (-40,0)]
+
+segments = []
+
+for _ in start_shape:
+    a = t.Turtle("square")
+    a.color("white")
+    a.goto(_)
+    segments.append(a)
+
+new_shape=[]
 
 for _ in range(len(start_shape)):
-    tim.setpos(start_shape[_])
+    tup = start_shape[-_-1][0], start_shape[-_-1][1] + 20*_
+    new_shape.append(tup)
+
+for _ in new_shape:
+    a = t.Turtle("square")
+    a.color("white")
+    a.goto(_)
+    segments.append(a)
 
 
 screen.exitonclick()
