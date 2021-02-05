68a3c11c7700ff0786c7c3e87c14ffeed5f65c56
Jan Fiser <jan.fiser2@akka.eu>
2021-02-05 17:06:00 +0100

    Snake basic

--- main.py
+++ main.py
@@ -1,41 +1,69 @@
 import turtle as t
-import random
+import time
 
 screen = t.Screen()
 screen.setup(width=500, height=400, )
-
 screen.bgcolor("black")
 
-turtles = []
-
-tim = t.Turtle()
-tim.shape("circle")
-tim.color("white")
-tim.pensize()
-tim.pensize(20)
-tim.speed(1)
-
-start_shape = [(0, 0), (-20, 0), (-40,0)]
+start_shape = [(0, 0), (20, 0), (40,0), (60,0)]
 
 segments = []
 
-for _ in start_shape:
-    a = t.Turtle("square")
-    a.color("white")
-    a.goto(_)
-    segments.append(a)
+for pos in start_shape:
+    newseg = t.Turtle()
+    newseg.color("white")
+    newseg.shape("square")
+    newseg.penup()
+    newseg.goto(pos)
+    segments.append(newseg)
 
-new_shape=[]
+def terin():
+    if screen.onkeypress() == "w":
+        return tur.forward(20)
+    elif screen.onkeypress() == "s":
+        return tur.backward(20)
+    elif screen.onkeypress() == "d":
+        return tur.right(20)
+    elif screen.onkeypress() == "a":
+        return tur.left(20)
+    else:
+        return tur.forward(20)
 
-for _ in range(len(start_shape)):
-    tup = start_shape[-_-1][0], start_shape[-_-1][1] + 20*_
-    new_shape.append(tup)
 
-for _ in new_shape:
-    a = t.Turtle("square")
-    a.color("white")
-    a.goto(_)
-    segments.append(a)
+
+gameOn = True
+while gameOn:
+    screen.update()
+    time.sleep(0.5)
+
+    for seg in range(len(segments)-1, 0 ,-1):
+        newx = segments[seg - 1].xcor()
+        newy = segments[seg -1 ].ycor()
+        segments[seg].goto(newx, newy)
+
+    terin(segments[0])
+
+
+
+
+
+# for _ in start_shape:
+#     a = t.Turtle("square")
+#     a.color("white")
+#     a.goto(_)
+#     segments.append(a)
+#
+# new_shape=[]
+#
+# for _ in range(len(start_shape)):
+#     tup = start_shape[-_-1][0], start_shape[-_-1][1] + 20*_U
+#     new_shape.append(tup)
+#
+# for _ in new_shape:
+#     a = t.Turtle("square")
+#     a.color("white")
+#     a.goto(_)
+#     segments.append(a)
 
 
 screen.exitonclick()
