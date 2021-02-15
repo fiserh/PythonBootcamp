1edc3aae78c4dd9769aa9c410b480fd3d16cd991
f <fiser.h@gmail.com>
2021-02-15 16:27:38 +0100

    data basic

--- main.py
+++ main.py
@@ -1,15 +1,5 @@
-import turtle as t
-import random
+# import pandas
+#
+# data = pandas.read_csv("weather_data.csv")
 
-tim = t.Turtle()
-colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
-
-def draw_shape(num_sides):
-    angle = 360 / num_sides
-    for _ in range(num_sides):
-        tim.forward(100)
-        tim.right(angle)
-
-for shape_side_n in range(11, 20):
-    tim.color(random.choice(colours))
-    draw_shape(shape_side_n)
+print("waka")
